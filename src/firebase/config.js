d7c91eb2ffd5f894869fc8f993324680d7b22041
Jinnie-kim <hjk7639@gmail.com>
2022-09-07 23:19:43 +0900

    firebase 프로젝트 설정

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7XCZsLNNRgvio-o-IBmUX6C-xF-rRLng",
+  authDomain: "my-diary-f4eaa.firebaseapp.com",
+  projectId: "my-diary-f4eaa",
+  storageBucket: "my-diary-f4eaa.appspot.com",
+  messagingSenderId: "316292753573",
+  appId: "1:316292753573:web:028fb963a4fc08d6aa054c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
