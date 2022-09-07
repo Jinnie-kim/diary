88e5b4f71e78e5c343428e9668f469b41dd164b9
Jinnie-kim <hjk7639@gmail.com>
2022-09-07 22:51:45 +0900

    로그인 폼 UI, 스타일링

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,47 @@
+import { useState } from 'react';
 import styles from './Login.module.css';
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const userDataHandler = (event) => {
+    if (event.target.type === 'email') {
+      setEmail(event.target.value);
+    } else if (event.target.type === 'password') {
+      setPassword(event.target.value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className={styles.login_form} onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>LOGIN</legend>
+        <label htmlFor="myEmail">Email : </label>
+        <input
+          type="email"
+          id="myEmail"
+          value={email}
+          required
+          onChange={userDataHandler}
+        />
+        <label htmlFor="myPassword">Password : </label>
+        <input
+          type="password"
+          id="myPassword"
+          value={password}
+          required
+          onChange={userDataHandler}
+        />
+        <button type="submit" className={styles.button}>
+          LOGIN
+        </button>
+      </fieldset>
+    </form>
+  );
 }
