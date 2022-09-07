867b8fc63a926bd73fcb313fe9eedf8a58dabc0e
Jinnie-kim <hjk7639@gmail.com>
2022-09-07 21:26:49 +0900

    다이어리 기본 페이지들 라우팅 하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from '../src/pages/home/Home';
+import Login from '../src/pages/login/Login';
+import Signup from '../src/pages/signup/Signup';
+
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
