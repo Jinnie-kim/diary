16127a94999fcd57c34bc1b1b2a97422e407afba
Jinnie-kim <hjk7639@gmail.com>
2022-09-07 21:51:11 +0900

    Nav 컴포넌트 App컴포넌트에 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Nav from './components/Nav';
 import Home from '../src/pages/home/Home';
 import Login from '../src/pages/login/Login';
 import Signup from '../src/pages/signup/Signup';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />}></Route>
