ea550686c035bc7a142b3217dc584d484e916091
Jinnie-kim <hjk7639@gmail.com>
2022-09-07 23:11:18 +0900

    회원가입 폼 UI, 스타일링

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,60 @@
+import { useState } from 'react';
+
 import styles from './Signup.module.css';
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const userDataHandler = (event) => {
+    if (event.target.type === 'email') {
+      setEmail(event.target.value);
+    } else if (event.target.type === 'password') {
+      setPassword(event.target.value);
+    } else if (event.target.type === 'text') {
+      setDisplayName(event.target.value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form className={styles.signup_form} onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>JOIN</legend>
+        <label htmlFor="myEmail">Email : </label>
+        <input
+          type="email"
+          id="myEmail"
+          value={email}
+          required
+          onChange={userDataHandler}
+        />
+        <label htmlFor="myPassword">Password : </label>
+        <input
+          type="password"
+          id="myPassword"
+          value={password}
+          required
+          onChange={userDataHandler}
+        />
+
+        <label htmlFor="myNickname">Nickname : </label>
+        <input
+          type="text"
+          id="myNickname"
+          value={displayName}
+          required
+          onChange={userDataHandler}
+        />
+        <button type="submit" className={styles.button}>
+          JOIN
+        </button>
+      </fieldset>
+    </form>
+  );
 }
