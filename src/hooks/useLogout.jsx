caa3d2f97d4d73fda8534988bcc4a4fedce58911
Jinnie-kim <hjk7639@gmail.com>
2022-09-08 11:45:23 +0900

    로그아웃 커스텀 hook

---
+++ src/hooks/useLogout.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { appAuth } from '../firebase/config';
+import { signOut } from 'firebase/auth';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = () => {
+    setError(null);
+    setIsPending(true);
+
+    signOut(appAuth)
+      .then(() => {
+        dispatch({ type: 'logout' });
+        setError(null);
+        setIsPending(false);
+      })
+      .catch((error) => {
+        setError(error.messages);
+        setIsPending(false);
+      });
+  };
+
+  return { error, isPending, logout };
+};
