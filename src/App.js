6b24fbe0965378c05cdf213fbc75c4fded18685f
Jinnie-kim <hjk7639@gmail.com>
2022-09-10 13:45:45 +0900

    firebase와 통신 후 화면이 렌더링되는 로직 추가

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Nav from './components/Nav';
 import Home from '../src/pages/home/Home';
 import Login from '../src/pages/login/Login';
 import Signup from '../src/pages/signup/Signup';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { isAuthReady } = useAuthContext();
   return (
     <div>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-        </Routes>
-      </BrowserRouter>
+      {isAuthReady ? (
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        'Loading ...'
+      )}
     </div>
   );
 }
