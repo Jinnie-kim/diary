4dbb2c0848fba17de87f3ef8990404fb32d63209
Jinnie-kim <hjk7639@gmail.com>
2022-09-10 15:32:07 +0900

    유저 정보에 따른 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Nav from './components/Nav';
 import Home from '../src/pages/home/Home';
@@ -7,16 +7,27 @@ import Signup from '../src/pages/signup/Signup';
 import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
-  const { isAuthReady } = useAuthContext();
+  const { isAuthReady, user } = useAuthContext();
   return (
     <div>
       {isAuthReady ? (
         <BrowserRouter>
           <Nav />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/signup" element={<Signup />}></Route>
+            <Route
+              path="/"
+              element={
+                user ? <Home /> : <Navigate replace={true} to="/login" />
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate replace={true} to="/" />}
+            ></Route>
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate replace={true} to="/" />}
+            ></Route>
           </Routes>
         </BrowserRouter>
       ) : (
