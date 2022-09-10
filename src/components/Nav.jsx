1c399f67449fa013ae18ced108587e449ec5e57e
Jinnie-kim <hjk7639@gmail.com>
2022-09-10 11:51:23 +0900

    로그인 여부에 맞는 UI 변경

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
 import { useLogout } from '../hooks/useLogout';
 
 import styles from './Nav.module.css';
 
 export default function Nav() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <nav className={styles.nav}>
       <h1 className={styles.title}>Secret Diary👻</h1>
       <ul className={styles.list_nav}>
-        <li>
-          <Link to="/login">LOGIN</Link>
-        </li>
-        <li>
-          <Link to="/signup">JOIN</Link>
-        </li>
-        <li>
-          <button type="button" onClick={logout}>
-            LOGOUT
-          </button>
-        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link to="/login">LOGIN</Link>
+            </li>
+            <li>
+              <Link to="/signup">JOIN</Link>
+            </li>{' '}
+          </>
+        ) : (
+          <li>
+            <strong>💗Welcome {user.displayName}💗</strong>
+            <button type="button" onClick={logout}>
+              LOGOUT
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
