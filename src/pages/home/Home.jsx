ce2eb51f4ed63aa28859ad207f7e97f2aa3c3755
Jinnie-kim <hjk7639@gmail.com>
2022-09-10 17:24:38 +0900

    홈 컴포넌트 UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,14 @@
+import DiaryForm from './DiaryForm';
+
 import styles from './Home.module.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className={styles.container}>
+      <aside className={styles.side_menu}>
+        <DiaryForm />
+      </aside>
+      <ul className={styles.content_list}>diary list</ul>
+    </main>
+  );
 }
