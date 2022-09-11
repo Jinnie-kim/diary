5beb18f6fed4e021fda514b4d1594afa2f34e5be
Jinnie-kim <hjk7639@gmail.com>
2022-09-11 16:02:12 +0900

    diary list 삭제 버튼 구현

--- src/pages/home/DiaryList.jsx
+++ src/pages/home/DiaryList.jsx
@@ -1,6 +1,8 @@
+import { useFireStore } from '../../hooks/useFireStore';
 import styles from './Home.module.css';
 
 export default function DiaryList({ diaries }) {
+  const { deleteDocument } = useFireStore('diary');
   return (
     <>
       {diaries.map((diary) => {
@@ -8,6 +10,14 @@ export default function DiaryList({ diaries }) {
           <li key={diary.id}>
             <strong className={styles.title}>{diary.title}</strong>
             <p className={styles.content}>{diary.content}</p>
+            <button
+              type="button"
+              onClick={() => {
+                deleteDocument(diary.id);
+              }}
+            >
+              DEL
+            </button>
           </li>
         );
       })}
