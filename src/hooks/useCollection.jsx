b8414e0d19647d126e3ebc9a8db0a8419f01b17c
Jinnie-kim <hjk7639@gmail.com>
2022-09-11 12:05:27 +0900

    firestore query 로직 추가

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import { appFireStore } from '../firebase/config';
 
-export const useCollection = (transaction) => {
+export const useCollection = (transaction, myQuery) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    let q;
+    if (myQuery) {
+      q = query(collection(appFireStore, transaction), where(...myQuery));
+    }
     const unsubscrieb = onSnapshot(
-      collection(appFireStore, transaction),
+      myQuery ? q : collection(appFireStore, transaction),
       (snapshot) => {
         let result = [];
         snapshot.docs.forEach((doc) => {
