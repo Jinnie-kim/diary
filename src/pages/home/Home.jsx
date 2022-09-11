1a86e1d75836d7ecbeab56b001108fce21d44dc6
Jinnie-kim <hjk7639@gmail.com>
2022-09-11 11:38:28 +0900

    홈 컴포넌트에 다이어리 리스트 컴포넌트 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,23 @@
 import DiaryForm from './DiaryForm';
+import DiaryList from './DiaryList';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 import styles from './Home.module.css';
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection('diary');
 
   return (
     <main className={styles.container}>
       <aside className={styles.side_menu}>
         <DiaryForm uid={user.uid} />
       </aside>
-      <ul className={styles.content_list}>diary list</ul>
+      <ul className={styles.content_list}>
+        {error && <strong>{error}</strong>}
+        {documents && <DiaryList diaries={documents} />}
+      </ul>
     </main>
   );
 }
