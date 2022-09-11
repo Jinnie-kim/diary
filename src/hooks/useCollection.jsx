f28e366d3edec61c4f72dbdad72f053796d8e851
Jinnie-kim <hjk7639@gmail.com>
2022-09-11 11:30:04 +0900

    누락된 useState import

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { collection, onSnapshot } from 'firebase/firestore';
 import { appFireStore } from '../firebase/config';
 
